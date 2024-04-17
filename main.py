62596a45162e85ef3149ef4ceda5091a47d3e152
scout <Scoutgolddrummer@gmail.com>
2024-04-17 18:25:05 -0400

    trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while (True):
-    print("Hello WOrld") 
-testing jteai j
+    print("Hello WOrld")
+(Michael edit this line )testing jteai j
 
 
 THIS IS A BIG CHANGE (Nuh uh)
@@ -8,3 +8,5 @@ THIS IS A BIG CHANGE (Nuh uh)
 
 HIIIIIIII
 intersting...
+
+Michael is eddit here
